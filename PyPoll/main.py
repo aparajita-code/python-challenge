781d041f00b6522dc09b48c2c38fbac2a8b9bc3b
Aparajita Mondal <write2a.mondal@gmail.com>
2024-07-18 18:22:15 -0500

    code without file write

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,6 +1,38 @@
 import csv
+
+from collections import Counter
+
 #reading the csv file
-with open("Resources/election_data.csv", mode='r') as file:
-    csvFile = csv.reader(file)
-    for lines in csvFile:
-        print(lines)
+with open('Resources/election_data.csv', 'r') as csvfile:
+    # Reading the CSV file Done
+    csvreader = csv.reader(csvfile)
+    next(csvreader)
+    candidates = [row[2] for row in csvreader]
+    #Count occurrences of each value in the second column
+    counts = Counter(candidates)
+    total_rows = len(candidates)
+    winner = max(counts, key=counts.get)
+    # Print the summary
+    for value, count in counts.items():
+        percentage = (count / total_rows) * 100
+        print(f'{value}: {count} : {percentage}')
+
+
+    print(f"Winner : {winner}")
+
+    print("Election Results")
+    print("------------------------")
+    print(f"Total number of votes:{total_rows}")
+
+
+
+
+
+
+
+
+
+
+
+
+
