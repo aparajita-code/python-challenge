0bb84a7e97b6f66fcae5f1f32a78942691d9fbd2
aparajita-code <write2a.mondal@gmail.com>
2024-07-18 23:46:51 -0500

    Update main.py
    Updated the write file section for Pybank

--- PyBank/main.py
+++ PyBank/main.py
@@ -57,5 +57,18 @@ print(f"Greatest Increase in Profits: {max_increase_date} (${max_increase})")
 print(f"Greatest Decrease in Profits: {max_decrease_date} (${max_decrease})")
 
 
+with open('Resources/bank_output.csv', 'w', newline='') as outfile:
+    # vScode needs entire path of the file it seems
+    # Write a single row
+    outfile.write("Financial Analysis \n")
+    outfile.write("------------------------\n")
+    outfile.write(f"Total Months :{total_months}\n")
+    outfile.write(f"Total: ${net_total}")
+    outfile.write(f"Average Change :${average_change:.2f}\n")
+    outfile.write("------------------------\n")
+    outfile.write(f"Greatest Increase in Profits: {max_increase_date} (${max_increase})\n")
+    outfile.write(f"Greatest Decrease in Profits: {max_decrease_date} (${max_decrease})")
+
+
 
 
